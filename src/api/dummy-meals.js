a973179c8c74ee790d7650c2b0b90a25aabcfab5
Donny Rendi <donny.rendi@yahoo.co.id>
2023-10-17 17:09:54 +0700

    refreshing DUMMY_MEALS with new menu

--- src/api/dummy-meals.js
+++ src/api/dummy-meals.js
@@ -1,28 +1,53 @@
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbeque Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
+    name: 'Pasta Carbonara',
+    description: 'Spaghetti with creamy carbonara sauce and pancetta',
+    price: 14.99,
   },
   {
     id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
+    name: 'Caprese Salad',
+    description: 'Fresh mozzarella, ripe tomatoes, basil, and balsamic glaze',
+    price: 12.99,
+  },
+  {
+    id: 'm3',
+    name: 'Beef Tacos',
+    description: 'Three soft tacos with seasoned ground beef and toppings',
+    price: 10.99,
+  },
+  {
+    id: 'm4',
+    name: 'Greek Salad',
+    description: 'Crisp lettuce, feta cheese, olives, tomatoes, and Greek dressing',
+    price: 11.99,
+  },
+  {
+    id: 'm5',
+    name: 'Margarita Pizza',
+    description: 'Classic Italian pizza with tomato and cheese',
+    price: 14.99,
+  },
+  {
+    id: 'm6',
+    name: 'Caesar Salad',
+    description: 'Crispy Romaine lettuce, croutons, and Caesar dressing',
+    price: 9.99,
+  },
+  {
+    id: 'm7',
+    name: 'Chicken Alfredo',
+    description: 'Creamy Alfredo sauce with grilled chicken and pasta',
+    price: 15.99,
+  },
+  {
+    id: 'm8',
+    name: 'Vegetarian Stir-Fry',
+    description: 'Assorted vegetables stir-fried in a savory sauce',
+    price: 13.49,
   },
 ];
+
 
 export default DUMMY_MEALS;
